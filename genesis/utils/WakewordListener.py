466840517facf0eabb9fe69d9ab741c8b0763cb4
DESKTOP <admin@xeiverse.co.uk>
2022-02-23 14:56:38 +0000

    Update WakewordListener.py

--- genesis/utils/WakewordListener.py
+++ genesis/utils/WakewordListener.py
@@ -1,17 +1,48 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import librosa
+import numpy as np
+from tensorflow.keras.models import load_model
 
+fs = 44100
+seconds = 2
+filename = "prediction.wav"
+class_names = ["Wake Word NOT Detected", "Wake Word Detected"]
 
 class WakewordListener:
 
     """
     Class for listening for the wakeword
-    E.g Genesis
+    E.g Genesis, Alexa, Hey Google
     """
 
     def __init__(self) -> None:
-        pass
+        self.listening = False
+        self.model = load_model("wake_word_models/Genesis.h5")
+
+    def _run_wake_word_detection(self) -> None:
+        while self.listening:
+            print("Say Now: ")
+            myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+            sd.wait()
+            write(filename, fs, myrecording)
+
+            audio, sample_rate = librosa.load(filename)
+            mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+            mfcc_processed = np.mean(mfcc.T, axis=0)
+
+            prediction = self.model.predict(np.expand_dims(mfcc_processed, axis=0))
+            if prediction[:, 1] > 0.99:
+                print(f"Wake Word Detected for ({i})")
+                print("Confidence:", prediction[:, 1])
+                i += 1
+            
+            else:
+                print(f"Wake Word NOT Detected")
+                print("Confidence:", prediction[:, 0])
 
     def start_listening(self):
         pass
 
     def stop_listening(self):
-        pass
+        self.listening = False
